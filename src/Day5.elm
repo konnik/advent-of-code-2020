a1b0894447c27eb8cc265c733c44580acd85815a
konnik <niklas.konstenius@gmail.com>
2020-12-05 15:43:50 +0100

    day 5 part 2

--- src/Day5.elm
+++ src/Day5.elm
@@ -1,5 +1,7 @@
 module Day5 exposing (solution)
 
+import Dict
+import Set exposing (Set)
 import Types exposing (Solution, Solver)
 
 
@@ -10,29 +12,49 @@ solution =
 
 part1 : Solver
 part1 input =
-    String.lines input
-        |> List.map seatId
-        |> List.maximum
-        |> Maybe.withDefault -1
+    input
+        |> parseInput
+        |> (List.maximum >> Maybe.withDefault -1)
         |> String.fromInt
 
 
 part2 : Solver
 part2 input =
-    "not implemented"
-
-
-seatId : String -> Int
-seatId pass =
     let
-        charToBit ch =
-            if ch == 'B' || ch == 'R' then
-                1
+        seatIds : Set Int
+        seatIds =
+            parseInput input |> Set.fromList
 
-            else
-                0
+        emptyWithNeightbours : Int -> Bool
+        emptyWithNeightbours seatId =
+            not (Set.member seatId seatIds)
+                && Set.member (seatId - 1) seatIds
+                && Set.member (seatId + 1) seatIds
     in
-    pass
+    List.range 0 1023
+        |> List.filter emptyWithNeightbours
+        |> (List.head >> Maybe.withDefault -1)
+        |> String.fromInt
+
+
+
+-- input parsing
+
+
+parseInput : String -> List Int
+parseInput input =
+    String.lines input
+        |> List.map toSeatId
+
+
+toSeatId : String -> Int
+toSeatId boardingPass =
+    boardingPass
         |> String.toList
         |> List.map charToBit
         |> List.foldl (\bit acc -> acc * 2 + bit) 0
+
+
+charToBit : Char -> Int
+charToBit ch =
+    Dict.get ch (Dict.fromList [ ( 'B', 1 ), ( 'R', 1 ) ]) |> Maybe.withDefault 0
